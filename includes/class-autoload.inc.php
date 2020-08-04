8beac335c70bf9d83b82e4d7fec9871cfc448bdd
likenhp <likenhp@gmail.com>
2020-08-04 13:57:34 -0700

    test

---
+++ includes/class-autoload.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+spl_autoload_register('autoLoad');
+
+function autoLoad ($className) {
+    $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+    $path = 'classes/';
+
+    if (strpos($url, 'includes')) {
+    $path = '../classes/';
+    }
+
+    $ext = '.class.php';
+    require_once $path . $className . $ext
+}
